9c6595d546b25214ebbf16ca9b9527e8e23a9988
yagamai <zodiake.wang@ihotkeys.com>
2016-01-04 15:40:42 +0800

    shopcoupon list feature

--- src/main/java/com/by/controller/AdminShopCouponController.java
+++ src/main/java/com/by/controller/AdminShopCouponController.java
@@ -1,5 +1,12 @@
 package com.by.controller;
 
+import com.by.form.ShopCouponForm;
+import com.by.json.ShopCouponJson;
+import com.by.model.Menu;
+import com.by.model.Shop;
+import com.by.model.ShopCoupon;
+import com.by.service.ShopCouponService;
+import com.by.service.ShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,13 +14,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.by.form.ShopCouponForm;
-import com.by.json.ShopCouponJson;
-import com.by.model.Menu;
-import com.by.service.ShopCouponService;
+import java.util.List;
 
 /**
  * Created by yagamai on 16-1-4.
@@ -28,6 +33,13 @@ public class AdminShopCouponController extends BaseController {
     private final Menu subMenu = new Menu(9);
     @Autowired
     private ShopCouponService service;
+    @Autowired
+    private ShopService shopService;
+
+    @ModelAttribute("shops")
+    public List<Shop> shops() {
+        return shopService.findAll(new Sort(Sort.Direction.ASC, "name"));
+    }
 
     // 列表页
     @RequestMapping(method = RequestMethod.GET)
@@ -39,6 +51,13 @@ public class AdminShopCouponController extends BaseController {
         uiModel.addAttribute("form", form);
         addMenu(uiModel);
         return LIST;
+    }
+
+    @RequestMapping(params = "form", method = RequestMethod.GET)
+    public String form(Model uiModel) {
+        uiModel.addAttribute("coupon", new ShopCoupon());
+        addMenu(uiModel);
+        return CREATE;
     }
 
     @Override
