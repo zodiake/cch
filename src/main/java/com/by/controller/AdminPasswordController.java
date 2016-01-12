03c98ac76fd9a7452ae6d1646cee34d3b63b36b0
yagamai <zodiake.wang@ihotkeys.com>
2016-01-12 15:27:07 +0800

    temp

--- src/main/java/com/by/controller/AdminPasswordController.java
+++ src/main/java/com/by/controller/AdminPasswordController.java
@@ -1,18 +1,18 @@
 package com.by.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.by.exception.Success;
 import com.by.json.PasswordJson;
 import com.by.model.Menu;
 import com.by.model.User;
 import com.by.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by yagamai on 16-1-7.
